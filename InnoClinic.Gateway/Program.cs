d25c99ac26ae9725507a89ef7b9c52f1e2ebb90c
S9392929 <apk59955@gmail.com>
2022-12-22 11:35:04 +0300

    Implement ocelot

--- InnoClinic.Gateway/Program.cs
+++ InnoClinic.Gateway/Program.cs
@@ -1,9 +1,17 @@
+using InnoClinic.Gateway;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.ConfigureJWTAuthentification(builder.Configuration);
+builder.Services.ConfigureCors();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,7 +26,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseOcelot();
 
 app.MapControllers();
 
---
+++ InnoClinic.Gateway/ServiceExstensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace InnoClinic.Gateway
+{
+    public static class ServiceExstensions
+    {
+        public static void ConfigureJWTAuthentification(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication("Bearer")
+                    .AddJwtBearer("Bearer", options =>
+                    {
+                        options.Authority = configuration.GetValue<string>("Routes:AuthorityRoute");
+                        options.Audience = "APIClient";
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateAudience = true,
+                            ValidAudience = "APIClient"
+                        };
+                    });
+        }
+
+        public static void ConfigureCors(this IServiceCollection services)
+            => services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+    }
+}
